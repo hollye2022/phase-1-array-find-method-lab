e6640665df345a8a7ef83901a4e08c2f5ef83d5c
chengying <hollye2558@gmail.com>
2022-11-06 20:43:43 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(array) {
+  let result = array.find(element => element.result === "W") 
+  if (result === undefined) {
+    return undefined;
+  }
+  return result.year;
+}
+
